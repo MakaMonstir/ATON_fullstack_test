7c03530e0635b901b3902b0255b78c41bbce37ec
Timur Usmanov <fonuatimur@gmail.com>
2024-06-14 11:36:55 +0700

    add routers

---
+++ app/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import login_view, clients_view, update_status
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('clients/', clients_view, name='clients'),
+    path('update_status/<int:client_id>/<str:status>/', update_status, name='update_status'),
+]
