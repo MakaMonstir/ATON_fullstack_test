e7b16b090dcfec239a75ab7408a0813e1639cce1
Timur Usmanov <fonuatimur@gmail.com>
2024-06-14 13:19:55 +0700

    add logout view

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_view, clients_view, update_status
+from .views import login_view, clients_view, update_status, logout_view
 
 urlpatterns = [
     path('', login_view, name='login'),
     path('clients/', clients_view, name='clients'),
     path('update_status/<int:client_id>/<str:status>/', update_status, name='update_status'),
+    path('logout/', logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Client
 from .forms import UserLoginForm
@@ -7,13 +7,13 @@ from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
     if request.user.is_authenticated:
-        return redirect('clients')  # Redirect if already logged in
+        return redirect('clients')
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('clients')  # Redirect to clients page after login
+            return redirect('clients')
     else:
         form = AuthenticationForm()
     return render(request, 'myapp/login.html', {'form': form})
@@ -29,3 +29,7 @@ def update_status(request, client_id, status):
     client.status = status
     client.save()
     return redirect('clients')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
